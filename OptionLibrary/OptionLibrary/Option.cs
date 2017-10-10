edfabb206a50563cbb21a23ca474832373ee9f7d
Andrey Kravtsun <avkravtsun@gmail.com>
2017-10-10 21:39:33 +0300

    Initial code commit.

---
+++ OptionLibrary/OptionLibrary/Option.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace OptionLibrary
+{
+    // Абстракция наличия или отсутствия значения
+    public class Option<T>
+    {
+        private readonly T _obj;
+        public bool IsSome { get; }
+        public bool IsNone => !IsSome;
+
+        public static Option<T> Some(T value)
+        {
+            return new Option<T>(value);
+        }
+
+        public static Option<T> None()
+        {
+            return new Option<T>();
+        }
+
+        public T Value()
+        {
+            if (IsNone)
+            {
+                throw new InvalidOperationException();
+            }
+            return _obj;
+        }
+
+        public Option<TU> Map<TU>(Func<T, TU> f)
+        {
+            return IsNone ? Option<TU>.None() : Option<TU>.Some(f(_obj));
+        }
+
+        public static Option<T> Flatten(Option<Option<T>> option)
+        {
+            return option.IsNone ? None() : option.Value();
+        }
+
+        private Option()
+        {
+            IsSome = false;
+        }
+
+        private Option(T value)
+        {
+            _obj = value;
+            IsSome = true;
+        }
+    }
+
+}
---
+++ OptionLibrary/OptionLibraryTests/OptionTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OptionLibrary;
+
+using IntOption = OptionLibrary.Option<int>;
+namespace OptionLibraryTests
+{
+    [TestClass]
+    public class OptionTests
+    {
+        private Func<int, int> _multiplier;
+        private IntOption _twoOption;
+        private IntOption _noneOption;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _multiplier = x => x * 2;
+            _twoOption = Option<int>.Some(2);
+            _noneOption = IntOption.None();
+        }
+
+        [TestMethod]
+        public void SomeTest()
+        {
+            Assert.IsTrue(_twoOption.IsSome);
+            Assert.IsFalse(_twoOption.IsNone);
+            Assert.AreEqual(2, _twoOption.Value());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ValueFailsOnEmptyTest()
+        {
+            Console.WriteLine($"none_value = {_noneOption.Value()}");
+        }
+
+        [TestMethod]
+        public void NoneTest()
+        {
+            Assert.IsTrue(_noneOption.IsNone);
+            Assert.IsFalse(_noneOption.IsSome);
+        }
+
+        [TestMethod]
+        public void ValueTest()
+        {
+            Assert.AreEqual(2, _twoOption.Value());
+        }
+
+        [TestMethod]
+        public void MapTest()
+        {
+            Assert.AreEqual(4, _twoOption.Map(_multiplier).Value());
+            Assert.IsTrue(_noneOption.Map(_multiplier).IsNone);
+        }
+
+        [TestMethod]
+        public void FlattenTest()
+        {
+            var someNoneOption = Option<IntOption>.Some(_noneOption);
+            Assert.IsTrue(IntOption.Flatten(someNoneOption).IsNone);
+
+            var noneNoneOption = Option<IntOption>.None();
+            Assert.IsTrue(noneNoneOption.IsNone);
+
+            var twoTwoOption = Option<IntOption>.Some(_twoOption);
+            Assert.IsTrue(twoTwoOption.IsSome);
+            Assert.AreEqual(2, IntOption.Flatten(twoTwoOption).Value());
+        }
+    }
+}
