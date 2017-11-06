05b1bed5364b31e426fb1ad3d2ffe524637a1270
Andrey Kravtsun <avkravtsun@gmail.com>
2017-11-06 18:30:42 +0300

    Shattered test methods.

--- OptionLibrary/OptionLibrary/Option.cs
+++ OptionLibrary/OptionLibrary/Option.cs
@@ -2,40 +2,86 @@
 
 namespace OptionLibrary
 {
+    public class OptionException : Exception
+    {
+        public OptionException()
+        {
+        }
+
+        public OptionException(string message)
+            : base(message)
+        {
+        }
+
+        public OptionException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+
     // Абстракция наличия или отсутствия значения
     public class Option<T>
     {
-        private readonly T _obj;
         public bool IsSome { get; }
         public bool IsNone => !IsSome;
 
-        public static Option<T> Some(T value)
+        public T Value
         {
-            return new Option<T>(value);
-        }
-
-        public static Option<T> None()
-        {
-            return new Option<T>();
-        }
-
-        public T Value()
-        {
-            if (IsNone)
+            get
             {
-                throw new InvalidOperationException();
+                if (IsNone)
+                {
+                    throw new OptionException("None object, cannot return Value()");
+                }
+                return _obj;
             }
-            return _obj;
         }
 
-        public Option<TU> Map<TU>(Func<T, TU> f)
+        private static readonly Option<T> NoneObject = new Option<T>();
+        private readonly T _obj;
+
+        public static Option<T> Some(T value) => new Option<T>(value);
+
+        public static Option<T> None() => NoneObject;
+
+        public Option<TU> Map<TU>(Func<T, TU> f) => IsNone ? Option<TU>.None() : Option<TU>.Some(f(Value));
+
+        public static Option<T> Flatten(Option<Option<T>> option) => option.IsNone ? None() : option.Value;
+
+        public override bool Equals(object rhs)
         {
-            return IsNone ? Option<TU>.None() : Option<TU>.Some(f(_obj));
+            Option<T> rhsOption = rhs as Option<T>;
+            if (rhsOption == null)
+            {
+                return false;
+            }
+            if (IsNone || rhsOption.IsNone)
+            {
+                return IsNone == rhsOption.IsNone;
+            }
+            return Equals(Value, rhsOption.Value);
         }
 
-        public static Option<T> Flatten(Option<Option<T>> option)
+        public override int GetHashCode() => Value == null ? 0 : Value.GetHashCode();
+
+        public static bool operator ==(Option<T> a, Option<T> b)
         {
-            return option.IsNone ? None() : option.Value();
+            if (ReferenceEquals(a, b))
+            {
+                return true;
+            }
+
+            if ((object) a == null || (object) b == null)
+            {
+                return false;
+            }
+
+            return Equals(a.Value, b.Value);
+        }
+
+        public static bool operator !=(Option<T> a, Option<T> b)
+        {
+            return !(a == b);
         }
 
         private Option()
@@ -49,5 +95,4 @@ namespace OptionLibrary
             IsSome = true;
         }
     }
-
 }
--- OptionLibrary/OptionLibraryTests/OptionTests.cs
+++ OptionLibrary/OptionLibraryTests/OptionTests.cs
@@ -16,57 +16,193 @@ namespace OptionLibraryTests
         public void SetUp()
         {
             _multiplier = x => x * 2;
-            _twoOption = Option<int>.Some(2);
+            _twoOption = IntOption.Some(2);
             _noneOption = IntOption.None();
         }
 
         [TestMethod]
-        public void SomeTest()
+        public void IsSomeReturnsTrueForSomeTest()
         {
             Assert.IsTrue(_twoOption.IsSome);
-            Assert.IsFalse(_twoOption.IsNone);
-            Assert.AreEqual(2, _twoOption.Value());
         }
 
         [TestMethod]
-        [ExpectedException(typeof(InvalidOperationException))]
+        public void IsNoneReturnsFalseForSomeTest()
+        {
+            Assert.IsFalse(_twoOption.IsNone);
+        }
+
+        [TestMethod]
+        public void SomeValueIsExtractibleTest()
+        {
+            Assert.AreEqual(2, _twoOption.Value);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OptionException))]
         public void ValueFailsOnEmptyTest()
         {
-            Console.WriteLine($"none_value = {_noneOption.Value()}");
+            Console.WriteLine($"none_value = {_noneOption.Value}");
         }
 
         [TestMethod]
-        public void NoneTest()
+        public void IsNoneReturnsTrueForNoneTest()
         {
             Assert.IsTrue(_noneOption.IsNone);
+        }
+
+        [TestMethod]
+        public void IsSomeReturnsFalseForNoneTest()
+        {
             Assert.IsFalse(_noneOption.IsSome);
         }
 
         [TestMethod]
         public void ValueTest()
         {
-            Assert.AreEqual(2, _twoOption.Value());
+            Assert.AreEqual(2, _twoOption.Value);
         }
 
         [TestMethod]
         public void MapTest()
         {
-            Assert.AreEqual(4, _twoOption.Map(_multiplier).Value());
+            Assert.AreEqual(4, _twoOption.Map(_multiplier).Value);
             Assert.IsTrue(_noneOption.Map(_multiplier).IsNone);
         }
 
         [TestMethod]
-        public void FlattenTest()
+        public void SomeSomeFlattenedIntoSomeTest()
+        {
+            var twoTwoOption = Option<IntOption>.Some(_twoOption);
+            Assert.IsTrue(twoTwoOption.IsSome);
+            Assert.AreEqual(2, IntOption.Flatten(twoTwoOption).Value);
+        }
+
+        [TestMethod]
+        public void SomeNoneFlattenedIntoNoneTest()
         {
             var someNoneOption = Option<IntOption>.Some(_noneOption);
             Assert.IsTrue(IntOption.Flatten(someNoneOption).IsNone);
+        }
 
+        [TestMethod]
+        public void NoneNoneFlattenedIntoNoneTest()
+        {
             var noneNoneOption = Option<IntOption>.None();
             Assert.IsTrue(noneNoneOption.IsNone);
+        }
 
-            var twoTwoOption = Option<IntOption>.Some(_twoOption);
-            Assert.IsTrue(twoTwoOption.IsSome);
-            Assert.AreEqual(2, IntOption.Flatten(twoTwoOption).Value());
+        [TestMethod]
+        public void NoneReferenceEqualsNoneTest()
+        {
+            var firstNone = IntOption.None();
+            var secondNone = IntOption.None();
+            Assert.IsTrue(firstNone == secondNone);
+        }
+
+        [TestMethod]
+        public void SomeEqualsSomeTest()
+        {
+            Assert.AreEqual(_twoOption.Map(x => x * 2), IntOption.Some(4));
+        }
+
+        [TestMethod]
+        public void EqualsReflexivityHoldsForNoneTest()
+        {
+            Assert.AreEqual(_noneOption, _noneOption);
+        }
+
+        [TestMethod]
+        public void EqualsReflexivityHoldsForSomeTest()
+        {
+            Assert.AreEqual(_twoOption, _twoOption);
+        }
+
+        [TestMethod]
+        public void EqualsCommutativityHoldsTest()
+        {
+            var a = IntOption.None();
+            var b = IntOption.None();
+            Assert.AreEqual(a, b);
+            Assert.AreEqual(b, a);
+            var three = IntOption.Some(3);
+            Assert.AreNotEqual(_twoOption, three);
+            Assert.AreNotEqual(three, _twoOption);
+        }
+
+        [TestMethod]
+        public void SomeOperatorEqualsSomeTest()
+        {
+            var mappedFour = _twoOption.Map(x => x * 2);
+            var simpleFour = IntOption.Some(4);
+            Assert.IsTrue(mappedFour == simpleFour);
+            Assert.IsFalse(mappedFour != simpleFour);
+        }
+
+        [TestMethod]
+        public void SomeNotEqualsNullTest()
+        {
+            Assert.AreNotEqual(_twoOption, null);
+            Assert.IsFalse(_twoOption == null);
+            Assert.IsTrue(_twoOption != null);
+        }
+
+        [TestMethod]
+        public void NullNotEqualsSomeTest()
+        {
+            Assert.AreNotEqual(null, _twoOption);
+            Assert.AreNotEqual(_twoOption, null);
+        }
+
+        [TestMethod]
+        public void NullOperatorNotEqualsSomeTest()
+        {
+            Assert.IsTrue(null != _twoOption);
+        }
+
+        [TestMethod]
+        public void NullOperatorEqualsSomeTest()
+        {
+            Assert.IsFalse(null == _twoOption);
+        }
+
+        [TestMethod]
+        public void DifferentTypesNotEqualTest()
+        {
+            var intOne = IntOption.Some(1);
+            Assert.AreNotEqual(Option<bool>.Some(true), intOne);
+            Assert.AreNotEqual(intOne, Option<short>.Some(1));
+        }
+
+        [TestMethod]
+        public void EqualsPersistsOnCastTest()
+        {
+            var intOne = IntOption.Some(1);
+            object objectOne = intOne;
+            Assert.AreEqual(objectOne, intOne);
+            Assert.IsTrue((IntOption)objectOne == intOne);
+            Assert.IsFalse((IntOption)objectOne != intOne);
+        }
+
+        [TestMethod]
+        public void NoneNotEqualSomeTest()
+        {
+            var one = IntOption.Some(1);
+            Assert.AreNotEqual(one, _noneOption);
+            Assert.AreNotEqual(_noneOption, one);
+        }
+
+        [TestMethod]
+        public void HachCodeEqualsTest()
+        {
+            Assert.AreEqual(_twoOption.GetHashCode(), IntOption.Some(2).GetHashCode());
+        }
+
+        [TestMethod]
+        public void HashCodeNotEqualsTest()
+        {
+            // very unlikely to fail on correct GetHashCode implementation...
+            Assert.AreNotEqual(_twoOption, IntOption.Some(3));
         }
     }
 }
