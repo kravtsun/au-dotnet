bf97fb186235e19c004088a3445f6e2b6e76d964
Andrey Kravtsun <avkravtsun@gmail.com>
2017-12-03 22:20:28 +0300

    Primes.

---
+++ FSharp/Primes.fs
@@ -0,0 +1,9 @@
+﻿namespace FSharpHW
+
+module Primes = 
+    // natural series starting from 2.
+    let naturalSeries2 = Seq.initInfinite (fun x -> x + 2)
+
+    let infinite = 
+        let isPrime x = Seq.forall (fun y -> x % y <> 0) (Seq.takeWhile (fun y -> y < x) naturalSeries2)
+        Seq.filter isPrime naturalSeries2
