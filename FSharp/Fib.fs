5c275e3148b26a670b03e0dc8be84420137dc1e7
Andrey Kravtsun <avkravtsun@gmail.com>
2017-12-03 20:41:44 +0300

    fibonacci task and project structure.

---
+++ FSharp/Fib.fs
@@ -0,0 +1,17 @@
+﻿namespace FSharpHW
+
+open System
+
+module Fib =
+    let fib n = 
+        if n < 0 then
+            let message = sprintf "Invalid argument for fib: %d" n
+            failwith message
+        let rec fibHelper n = 
+                if n = 0 then
+                    (0, 1)
+                else
+                    let (prev, prevprev) = fibHelper (n - 1)
+                    prevprev, prev + prevprev
+        fst (fibHelper n)
+            
---
+++ FSharp/Program.fs
@@ -0,0 +1,10 @@
+﻿namespace FSharpHW
+open Fib
+module Program =
+    [<EntryPoint>]
+    let main argv = 
+    //    printfn "%A" argv
+        let fib1 = fib 1
+        let result = List.map (fun i -> printfn "fib(%d) = %d" i (fib i)) [0..10]
+        printfn "%d" fib1
+        0 // return an integer exit code
